05dfa3713752a82179c894efa05920b6243c2a75
Colokol <Colokol1@gmail.com>
2023-10-23 09:59:52 +0300

    Initial Commit

---
+++ FindClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindClone
+//
+//  Created by Uladzislau Yatskevich on 23.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
